98ed14695aebcbb784a073394dcc5512a12a8527
BoolinBoolean <57597848+BoolinBoolean@users.noreply.github.com>
2023-11-16 13:47:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { useState } from "react"; 
-  
-// Importing app.css is css file to add styling 
 import "./App.css"; 
   
 const App = () => { 
-  //  Counter is a state initialized to 0 
   const [counter, setCounter] = useState(0) 
   
-  // Function is called everytime increment button is clicked 
   const handleClick1 = () => { 
-    // Counter state is incremented 
     setCounter(counter + 1) 
   } 
   
-  // Function is called everytime decrement button is clicked 
   const handleClick2 = () => { 
-    // Counter state is decremented 
     setCounter(counter - 1) 
   } 
 

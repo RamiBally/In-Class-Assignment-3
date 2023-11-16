92b144812ab059c38054ff9466c0c0d7621ede40
BoolinBoolean <ramibally@gmail.com>
2023-11-16 13:45:01 -0500

    we've never been so back

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"; 
+  
+// Importing app.css is css file to add styling 
+import "./App.css"; 
+  
+const App = () => { 
+  //  Counter is a state initialized to 0 
+  const [counter, setCounter] = useState(0) 
+  
+  // Function is called everytime increment button is clicked 
+  const handleClick1 = () => { 
+    // Counter state is incremented 
+    setCounter(counter + 1) 
+  } 
+  
+  // Function is called everytime decrement button is clicked 
+  const handleClick2 = () => { 
+    // Counter state is decremented 
+    setCounter(counter - 1) 
+  } 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return ( 
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      fontSize: '300%', 
+      position: 'absolute', 
+      width: '100%', 
+      height: '100%', 
+      top: '-15%', 
+    }}> 
+      CSI3150 In Class Assignment 3 Counting App 
+      <div style={{ 
+        fontSize: '120%', 
+        position: 'relative', 
+        top: '10vh', 
+      }}> 
+        {counter} 
+      </div> 
+      <div className="buttons"> 
+        <button style={{ 
+          fontSize: '60%', 
+          position: 'relative', 
+          top: '20vh', 
+          marginRight: '5px', 
+          backgroundColor: 'green', 
+          borderRadius: '8%', 
+          color: 'white', 
+        }} 
+          onClick={handleClick1}>Increase</button> 
+        <button style={{ 
+          fontSize: '60%', 
+          position: 'relative', 
+          top: '20vh', 
+          marginLeft: '5px', 
+          backgroundColor: 'red', 
+          borderRadius: '8%', 
+          color: 'white', 
+        }} 
+          onClick={handleClick2}>Decrease</button> 
+      </div> 
+    </div> 
+  ) 
+} 
 
-export default App;
+export default App
